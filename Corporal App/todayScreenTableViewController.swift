d18476fef2ce3a47ecf19dad8855595f6be3cba2
Ivan Lebed <johnny.swan1991@gmail.com>
2022-01-28 13:46:54 +0300

    delete some bad words :-)

---
+++ Corporal App/todayScreenTableViewController.swift
@@ -0,0 +1,99 @@
+//
+//  todayScreenControllerTableViewController.swift
+//  Corporal App
+//
+//  Created by John Swan on 15.08.2021.
+//
+
+import UIKit
+
+class todayScreenTableViewController: UITableViewController {
+    let model = Corporal_App.defaultTimeTable
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Table view is loaded")
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return model.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TimeEventCell", for: indexPath) as! TimeEventCustomCell
+
+//         Configure the cell...
+        let timeTableItem = model[indexPath.row]
+        cell.titleLabel?.text = timeTableItem.title
+        let timeString = String(timeTableItem.beginTime.hours!)+":"+String(timeTableItem.beginTime.minutes!)
+
+        cell.timeStartLabel?.text = timeString
+        cell.timeEndLabel?.text = "00:00"
+
+        return cell
+    }
+    
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
